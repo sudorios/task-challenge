b47cb627f0fecf0561e198d683309c3657cd4959
sudorios <wtke90@gmail.com>
2025-09-12 12:40:10 -0500

    fix: set up url_prod

--- src/config/env.js
+++ src/config/env.js
@@ -2,8 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const env = {
-  PORT: process.env.PORT,
-  MONGODB_URI: process.env.MONGODB_URI,
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT || 4000,
   JWT_SECRET: process.env.JWT_SECRET,
+  MONGO_URI: process.env.MONGO_URI,
+  BACKEND_URL_DEV: process.env.BACKEND_URL_DEV,
   BACKEND_URL_PROD: process.env.BACKEND_URL_PROD,
 };
--- src/server.js
+++ src/server.js
@@ -3,10 +3,15 @@ dotenv.config();
 
 import app from './app.js';
 import { connectDB } from './config/db.js';
-import { env } from './config/env.js'; 
+import { env } from './config/env.js';
 
 connectDB().then(() => {
   app.listen(env.PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${env.PORT}`);
+    const baseUrl =
+      env.NODE_ENV === 'production'
+        ? env.BACKEND_URL_PROD
+        : env.BACKEND_URL_DEV;
+
+    console.log(`🚀 Servidor corriendo en ${baseUrl}`);
   });
 });
